571c718a69fa1233d0932d388ed93663ad02230c
tightwap20 <tightwap20@gmail.com>
2013-12-24 13:08:44 -0700

    Updated Javascript and embedded player

--- SoundCloud.js
+++ SoundCloud.js
@@ -1,41 +1,77 @@
-// function findMusicByTag(tag)
-// {
-// SC.initialize({
-//     client_id: '795b1eb8bc30f90032b1968b49583224'
-// });
-// SC.get('/tracks',{tags: tag},
-//     function(tracks)
-//     {
-//         console.log(tracks);
-//     });
-//}
   // SC.get('/tracks/293', function(track) {
   //       SC.oEmbed(track.permalink_url, document.getElementById('playerEmbed'));
   // });
-$(document).ready(function() {
-   
+$(document).ready(function() 
+{
+ var tagArray = []; 
+ $(".tags li").click(function()
+ {
+  var tag = ($(this).text());
+  tagArray.push(tag);
+  $(this).siblings().hide('slow');
+  console.log(tagArray);//console.log(tag);
+ });
+
    //When an li is clicked this function is thrown
-   $(".genre li").click(function() {
+   //var genreTwo = [];
+   $(".genre li").click(function() 
+   {
      $(this).siblings().hide('slow');      //hides the unclicked li's
      var genre = ($(this).text());         //sets genre equal to the the clicked li's text
-     console.log(genre);                   //logs the li text that was clicked
-     $(this).data('clicked', true);        // sets the clicked 
-     
-     if ($(".genre li").data('clicked')) {  //if the button is clicked, then run the below javascript
-       $.ajax({
- 	       dataType: "json",
-  		   type: "GET",
-  		   url: "http://api.soundcloud.com/tracks.json",
-  		   data: { client_id: "795b1eb8bc30f90032b1968b49583224", genres: genre }  //will pass variable clicked on to genre as well as tags
-	      })
+     //genreTwo.push(genre);
+     //console.log(genreTwo);                   //logs the li text that was clicked
+   
 
-       .done(function( tracks ) {
-  		   //console.dir(tracks[1].id);
-  		   for (var i = 0; i < tracks.length; i++) {
-    		   console.log(tracks[1].id);
-    		   console.log(tracks[1].title);
-  		    }
-        });
+   //var tagsArray;
+   $(".findMusic").click(function()
+   {
+      for (var i = 0; i < tagArray.length; ++i) 
+      {
+      if (tagArray.length == 1) 
+      {
+        var tagsArray = tagArray[i];
+        console.log(tagsArray);
       }
+      else if (tagArray.length > 1) 
+      {
+        var tagsArray = tagArray[i].concat( " " +  tagArray[i - 1]);
+        console.log(tagsArray);
+      }
+    }
+     $.ajax
+     ({
+ 	      dataType: "json",
+  		  type: "GET",
+  		  url: "http://api.soundcloud.com/tracks.json",
+  		  data:
+        { 
+          tags: tagsArray,
+          //q: tagsArray,
+          client_id: "795b1eb8bc30f90032b1968b49583224", 
+          genres: genre,
+          //tags: 
+        }  //will pass variable clicked on to genre as well as tags
+	   })
+
+      .done(function(tracks)
+      {
+  		  for (var j = 0; j < 5; j++) 
+        {
+    		  console.log(tracks[j].id);
+    		  console.log(tracks[j].title);
+          console.log(tracks[j].tag_list);
+          console.log(tracks[j].genre);
+  		  }
+          $(function()
+          {
+            $playerContainer = $('#player');
+            trackUrl = 'http://api.soundcloud.com/tracks/' + tracks[1].id;
+            $.getJSON('http://soundcloud.com/oembed?format=js&url=' + trackUrl + '&iframe=true&auto_play=true&callback=?', function(response)
+              {
+                $playerContainer.html(response.html);
+              });
+          });
+        });
+      });
     });
 });
